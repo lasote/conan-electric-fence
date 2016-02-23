61abba65fb00d4d316b7c84f0d84c913e49421ac
Lasote <lasote@gmail.com>
2016-02-23 20:03:50 +0100

    Conan package tools

--- build.py
+++ build.py
@@ -1,28 +1,39 @@
 import os
-import platform
+from conan.packager import ConanMultiPackager
 import sys
+import platform
+from copy import copy
+
 
 if __name__ == "__main__":
-    os.system('conan export lasote/stable')
-   
-    def test(settings):
-        argv =  " ".join(sys.argv[1:])
-        command = "conan test %s %s" % (settings, argv)
-        retcode = os.system(command)
-        if retcode != 0:
-            exit("Error while executing:\n\t %s" % command)
+    channel = os.getenv("CONAN_CHANNEL", "testing")
+    username = os.getenv("CONAN_USERNAME", "lasote")
+    current_page = os.getenv("CONAN_CURRENT_PAGE", "1")
+    total_pages = os.getenv("CONAN_TOTAL_PAGES", "1")
+    gcc_versions = os.getenv("CONAN_GCC_VERSIONS", None)
+    gcc_versions = gcc_versions.split(",") if gcc_versions else None
+    use_docker = os.getenv("CONAN_USE_DOCKER", False)
+    upload = os.getenv("CONAN_UPLOAD", False)
+    reference = os.getenv("CONAN_REFERENCE")
+    password = os.getenv("CONAN_PASSWORD")
+    travis = os.getenv("TRAVIS", False)
+    travis_branch = os.getenv("TRAVIS_BRANCH", None)
+    appveyor = os.getenv("APPVEYOR", False)
+    appveyor_branch = os.getenv("APPVEYOR_REPO_BRANCH", None)
 
-    # Static x86
-    test('-s arch=x86 -s build_type=Debug -o electric-fence:shared=False')
-    test('-s arch=x86 -s build_type=Release -o electric-fence:shared=False')
-    # Shared x86
-    test('-s arch=x86 -s build_type=Debug -o electric-fence:shared=True')
-    test('-s arch=x86 -s build_type=Release -o electric-fence:shared=True')
+    channel = "stable" if travis and travis_branch=="master" else channel
+    channel = "stable" if appveyor and appveyor_branch=="master" and not os.getenv("APPVEYOR_PULL_REQUEST_NUMBER") else channel
+    os.environ["CONAN_CHANNEL"] = channel # Override the environment value for test/conanfile.py file
 
-    # Static x86_64
-    test('-s arch=x86_64 -s build_type=Debug -o electric-fence:shared=False')
-    test('-s arch=x86_64 -s build_type=Release -o electric-fence:shared=False')
+    args = " ".join(sys.argv[1:])
+    builder = ConanMultiPackager(args, username, channel)
+    builder.add_common_builds(shared_option_name="electric-fence:shared", visual_versions=[10, 12, 14])
+    print(builder.builds)
 
-    # Shared x86_64
-    test('-s arch=x86_64 -s build_type=Debug -o electric-fence:shared=True')
-    test('-s arch=x86_64 -s build_type=Release -o electric-fence:shared=True')
+    if use_docker:  
+        builder.docker_pack(current_page, total_pages, gcc_versions)
+    else:
+        builder.pack(current_page, total_pages)
+
+    if upload and reference and password:
+        builder.upload_packages(reference, password)
