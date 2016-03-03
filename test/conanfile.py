d1a715fb0750013f0a0aa56c0dfdcf240f9f3b80
Lasote <lasote@gmail.com>
2016-03-03 10:17:30 +0100

    Fixed default channel

--- test/conanfile.py
+++ test/conanfile.py
@@ -3,12 +3,20 @@ from conans import CMake
 import os
 
 
+############### CONFIGURE THESE VALUES ##################
+default_user = "lasote"
+default_channel = "testing"
+#########################################################
+
+channel = os.getenv("CONAN_CHANNEL", default_channel)
+username = os.getenv("CONAN_USERNAME", default_user)
+
 class DefaultNameConan(ConanFile):
     name = "DefaultName"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
-    requires = "electric-fence/2.2.0@lasote/stable"
+    requires = "electric-fence/2.2.0@%s/%s" % (username, channel)
 
     def build(self):
         cmake = CMake(self.settings)
